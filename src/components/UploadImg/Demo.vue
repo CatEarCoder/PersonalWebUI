e40a2d0635bb66ad363fe0f494e6009e4ca40e06
zmm <1294919039@qq.com>
2018-12-03 15:10:08 +0800

    first

---
+++ src/components/UploadImg/Demo.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="main">
+		<v-uploader></v-uploader>
+	</div>
+</template>
+
+<script>
+	import Uploader from "@/components/UploadImg/Index"
+	export default {
+    data() {
+      return {
+      }
+    },
+    methods:{
+    	
+    },
+    created(){
+    	
+    },
+    mounted(){
+    	
+    },
+    components:{
+    	"v-uploader":Uploader
+    }
+ }
+</script>
+
+<style>
+</style>
---
+++ src/components/UploadImg/Index.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="uploader-box">
+		<input type="file" accept="image/*" class="my-uploader__input">
+	</div>
+</template>
+
+<script>
+</script>
+
+<style lang="sass">
+	.uploader-box{
+		width:100px;
+		height:100px;
+		.my-uploader__input{
+			width: 100%;
+			height:100%;
+			opacity: 0;
+		}
+	}
+</style>
