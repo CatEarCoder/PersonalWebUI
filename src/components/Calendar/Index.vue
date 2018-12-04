2634208a3f80fe9b636322aa53c42bd5a08413f1
zmm <1294919039@qq.com>
2018-12-04 16:56:23 +0800

    日历农历

--- src/components/Calendar/Index.vue
+++ src/components/Calendar/Index.vue
@@ -16,7 +16,7 @@
  			<div class='ny1'> {{ item.year }} 年 {{ item.month < 10?"0"+item.month:item.month }} 月</div>
  			<table class='dateTable'>
  				<tr v-for="row,index in Math.ceil(item.trData.length/7)">
- 					<td v-for="col,i in days" @click="selectDate(monthIndex,item.trData[index*7+i],$event)" :data-set="item.year+'-'+item.month+'-'+item.trData[index*7+i]" v-bind:class="{'active today':item.trData[index*7+i]==today&&monthIndex==0,'gray':item.trData[index*7+i]<today&&monthIndex==0}">{{ item.month+"-"+item.trData[index*7+i] | dateText }}</td>
+ 					<td v-for="col,i in days" @click="selectDate(monthIndex,item.trData[index*7+i],$event)" :data-set="item.year+'-'+item.month+'-'+item.trData[index*7+i]" v-bind:class="{'active today':item.trData[index*7+i]==today&&monthIndex==0,'gray':item.trData[index*7+i]<today&&monthIndex==0}"><p>{{ item.month+"-"+item.trData[index*7+i] | dateText }}</p><p>{{ item.year + "-" +item.month + "-" + item.trData[index*7+i] | lunarday}}</p></td>
  				</tr>
  			</table>
  		</div>
@@ -24,6 +24,7 @@
 	</div>
 </template>
 <script>
+	import lunarday from '@/utils/Lunarday';
 	let holiday = {"0101":"元旦","0214":"情人节","0308": "妇女节","0312":"植树节","0315": "消费者权益日","0401": "愚人节","0501": "劳动节","0512": "护士节","0601":"儿童节","0701": "建党节","0801": "建军节","0910": "教师节","1001": "国庆节","1224": "平安夜","1225":"圣诞节"};
 	let holiday1 = {"0101":"春节","0115": "元宵节","0505": "端午节","0707": "七夕节","0815": "中秋节","0909": "重阳节","1208": "腊八节","1224": "小年"};
 	export default{
@@ -58,11 +59,16 @@
 					text = "明天";
 				}
 				else{
-					
 					text = value.split("-")[1];
-					
 				}
 				return text;
+			},
+			lunarday(value){
+				if(value.indexOf("undefined")!=-1) return "";
+				let y = value.split("-")[0];
+				let m = value.split("-")[1];
+				let d = value.split("-")[2];
+				return lunarday.GetLunarDay(y,m,d).substr(2,2);
 			}
 		},
 		methods:{
@@ -324,8 +330,13 @@
 	}
 	td{
 		text-align:center;
-		height:30px;
-		line-height:30px;
+		padding:8px 0;
+		p{
+			height:20px;
+			line-height:20px;
+			font-size: 12px;
+			letter-spacing: 1px;
+		}
 	}
 }
 </style>
